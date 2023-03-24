ead05f3467d6da371e282f55e0c5c0e599728c88
Demonolog82 <68203431+Demonolog82@users.noreply.github.com>
2023-03-24 21:25:37 +0100

    Add files via upload

---
+++ eXoDOSDownloader.ps1
@@ -0,0 +1,18 @@
+﻿$uri = "https://the-eye.eu/public/Games/eXo/eXoDOS_v5/eXo/eXoDOS/?fbclid=IwAR2j41YvmFAyXyoJ1nJb40SrGndZHab1XGdWR-t4DDnn3JsIyGQb9GltleI"
+$result = Invoke-WebRequest -Uri $uri
+$uriFront = "https://the-eye.eu/public/Games/eXo/eXoDOS_v5/eXo/eXoDOS/"
+$games = $result.Links
+$localFolder = "C:\GamesDos"
+$gamesCount = $games | measure
+
+[int]$i = 0
+$game = @()
+
+foreach($game in $games){
+    $i++
+    if($game.outerText -notmatch "../"){
+        Write-Host "Game $($i) of $($gamesCount)"
+        Write-Host "Downloading $($game.outerText)..."
+        Invoke-WebRequest -Uri "$($uriFront)$($game.href)" -OutFile "$($localFolder)\$($game.outerText)"
+        }
+    }
